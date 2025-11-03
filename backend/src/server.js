2912e1c52955614b2d71fba6e6304ae6aa615f2b
Furkansegmenn <segmenfurkann@gmail.com>
2025-11-03 13:43:29 +0300

    db config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+(async () => {
+  try {
+    await connectDB();
+    
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+    
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+})();
+
+export default app;
