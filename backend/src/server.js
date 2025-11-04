6f2a235f7a817b28d6acb3dbc8e068ddaa2e0f57
Furkansegmenn <segmenfurkann@gmail.com>
2025-11-04 16:45:51 +0300

    trying

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-import {clerkMiddleware} from '@clerk/express';
+import { clerkMiddleware } from '@clerk/express';
 import cors from 'cors';
 
 import userRoute from './routes/user.route.js';
@@ -10,40 +10,25 @@ import commentRoute from './routes/comment.route.js';
 import notificationRoute from './routes/notification.route.js';
 import { arcjetMiddleware } from './middleware/arcjet.middleware.js';
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
-app.use(arcjetMiddleware)
+app.use(arcjetMiddleware);
 
-
-app.use('/api/users',userRoute);
-app.use('/api/posts', postRoute)
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 app.use('/api/comment', commentRoute);
 app.use('/api/notification', notificationRoute);
 
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel backend!');
+});
 
-(async () => {
-  try {
-    await connectDB();
-    
-    app.get('/', (req, res) => {
-      res.send('Hello World!');
-    });
-    
-   if(ENV.NODE_ENV !== 'production'){
-     app.listen(ENV.PORT, () => {
-      console.log(`Server is running on port ${ENV.PORT}`);
-    });
-   }
+await connectDB();
 
-
-  } catch (error) {
-    console.error('Failed to start server:', error);
-    process.exit(1);
-  }
-})();
+// ❌ app.listen yok!
+// çünkü Vercel kendi ortamında dinliyor
 
 export default app;
