eef7e53f4f6ee6c56006e92af4348d6cbb75d258
Furkansegmenn <segmenfurkann@gmail.com>
2025-11-05 11:11:44 +0300

    backend section finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-import { clerkMiddleware } from '@clerk/express';
+import {clerkMiddleware} from '@clerk/express';
 import cors from 'cors';
 
 import userRoute from './routes/user.route.js';
@@ -10,25 +10,35 @@ import commentRoute from './routes/comment.route.js';
 import notificationRoute from './routes/notification.route.js';
 import { arcjetMiddleware } from './middleware/arcjet.middleware.js';
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
-app.use(arcjetMiddleware);
+app.use(arcjetMiddleware)
 
-app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
+
+app.use('/api/users',userRoute);
+app.use('/api/posts', postRoute)
 app.use('/api/comment', commentRoute);
 app.use('/api/notification', notificationRoute);
 
-app.get('/', (req, res) => {
-  res.send('Hello from Vercel backend!');
-});
 
-await connectDB();
+(async () => {
+  try {
+    await connectDB();
+    
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+    
+   
 
-// ❌ app.listen yok!
-// çünkü Vercel kendi ortamında dinliyor
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+})();
 
 export default app;
