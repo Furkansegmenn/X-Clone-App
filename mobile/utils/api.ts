c6f3893aa4948d8ea20e4a7a9b4077136ba5da53
Furkansegmenn <segmenfurkann@gmail.com>
2025-11-13 13:39:26 +0300

    .

--- mobile/utils/api.ts
+++ mobile/utils/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { useAuth } from "@clerk/clerk-expo";
 
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
-// const API_BASE_URL = "https://localhost:5001"
+// const API_BASE_URL = "http://localhost:5001/"
 
 
 
